ff8da9daef61e21589793d9d2029057d2f879a2b
Prabhsimran Chhabra <chhabpra@sheridancollege.ca>
2016-04-04 19:59:58 -0400

    Fixed Invalid Blood Type Error. Initial Commit

---
+++ BloodBankProject/Donor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Popups;
+
+namespace BloodBankProject
+{
+    class Donor
+    {
+        public string _name;
+        public int _age;
+        public string _bloodType;
+        public string _gender;
+
+
+        public bool verifyAge(int donorAge)
+        {
+            if (donorAge < 18)
+            {
+                MessageDialog ageDlg = new MessageDialog("You have to be 18 or older to donate blood. Sorry!");
+                ageDlg.ShowAsync();
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+
+            
+        }
+
+        public bool verifyBloodType(string donorBloodType)
+        {
+            List<String> typesOfBlood = new List<string>();
+            typesOfBlood.Add("A+");
+            typesOfBlood.Add("A-");
+            typesOfBlood.Add("B+");
+            typesOfBlood.Add("B-");
+            typesOfBlood.Add("AB+");
+            typesOfBlood.Add("AB-");
+            typesOfBlood.Add("O+");
+            typesOfBlood.Add("O+");
+
+            if (typesOfBlood.Contains(donorBloodType))
+            {
+                return true;
+
+            }
+
+            else
+            {
+                MessageDialog bTypeDlg = new MessageDialog("Invalid Blood Type. Please Try Again!");
+                bTypeDlg.ShowAsync();
+                return false;
+            }
+
+
+        }
+    }
+
+    
+
+}
+
---
+++ BloodBankProject/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BloodBankProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+            
+        }
+
+        //Initialize a counter to count the number of donors
+        public int donorCounter = 0;
+        
+        //Initialize a list of donors to store donor objects.
+        List<Donor> listOfDonors = new List<Donor>();
+
+        private void onRegButtonClicked(object sender, RoutedEventArgs e)
+        {
+            //When Register Button is clicked, make the donor object and store its calues
+            
+            Donor bloodDonor = new Donor();
+            bloodDonor._name = donorNameBox.Text;
+            int checkAge = Int32.Parse(donorAgeBox.Text);
+            string checkBlood = donorBloodType.Text;
+
+
+            // Verify age: donors over age 18 are able to donate also verify blood type
+            if (bloodDonor.verifyAge(checkAge) && bloodDonor.verifyBloodType(checkBlood))
+
+            {
+                bloodDonor._age = Int32.Parse(donorAgeBox.Text);
+                bloodDonor._bloodType = donorBloodType.Text;
+                bloodDonor._gender = donorGenderBox.Text;
+                donorCounter += 1;
+                incrementDonorCountBlock.Text = donorCounter.ToString();
+                listOfDonors.Add(bloodDonor);
+
+            }
+          
+     
+        }
+
+        //Reset Button: Clear all values when user clicks the reset button.
+        private void OnResetButtonClicked(object sender, RoutedEventArgs e)
+        {
+            donorNameBox.Text = "";
+            donorAgeBox.Text = "";
+            donorBloodType.Text = "";
+            donorGenderBox.Text = "";
+            
+
+        }
+    }
+}
